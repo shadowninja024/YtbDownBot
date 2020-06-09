f0a0055ce35aafeff1ddedee1f4cd8aa2120ccb6
Denis Skovpen <deniskovpen@gmail.com>
2020-06-09 17:52:50 +0300

    Fix indent

--- src/av_utils.py
+++ src/av_utils.py
@@ -116,11 +116,11 @@ async def _media_size(url, session=None, http_headers=None):
     # try GET request when HEAD failed
     try:
         if content_length < 100:
-                async with _session.get(url, headers=http_headers) as get_resp:
-                    if get_resp.status != 200:
-                        raise Exception('Request failed: ' + str(get_resp.status) + " " + responses[get_resp.status])
-                    else:
-                        content_length = int(get_resp.headers.get(hdrs.CONTENT_LENGTH, '0'))
+            async with _session.get(url, headers=http_headers) as get_resp:
+                if get_resp.status != 200:
+                    raise Exception('Request failed: ' + str(get_resp.status) + " " + responses[get_resp.status])
+                else:
+                    content_length = int(get_resp.headers.get(hdrs.CONTENT_LENGTH, '0'))
     finally:
         if session is None:
             await _session.__aexit__(exc_type=None, exc_val=None, exc_tb=None)
